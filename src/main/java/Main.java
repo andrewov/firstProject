94dac945d14e726a318d4160418947854455ccbb
andrewov <nothing.type@gmail.com>
2018-06-27 14:12:53 +0300

    add my first test

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+
+    public int valueSumm(int a, int b){
+        return a + b;
+    }
+
+    public static int valueDiff(int a, int b){
+        return a - b;
+    }
+
+    public static int valueDivide(int a, int b){
+        int result;
+        result = a / b;
+        return result;
+    }
+
+    public static int valueMult(int a, int b){
+        return a*b;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void valueSumm() {
+        assertEquals(12, new Main().valueSumm(6,6));
+    }
+
+    @Test
+    public void valueDiff() {
+        assertEquals(5, new Main().valueDiff(12, 7));
+    }
+
+    @Test
+    public void valueDivide() {
+        assertEquals(7, new Main().valueDivide(21, 3) );
+    }
+
+    @Test
+    public void valueMult() {
+        assertEquals(125, new Main().valueMult(25, 5));
+    }
+}
