a577f58e74acf973a09c70c2dc4be0935212cd57
andrewov <nothing.type@gmail.com>
2018-07-05 21:58:50 +0300

    add HomeWork2 is succesfull

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         int[] array = {1,2,4,6,1,2,5};
+        String wordsLine = "";
         System.out.println(valueEvent(4, 4, 4));
         System.out.println("Координаты находятся в " + valueCoordinates(3, -5) + " четверти.");
         System.out.println("Факториал: " + valueFact(6));
@@ -13,6 +11,7 @@ public class Main {
         reversArray(array);
         System.out.println("-----------");
         bubleSort(array);
+
         for(int i = 0; i < array.length; i++){
             System.out.println(array[i]);
         }
@@ -22,7 +21,13 @@ public class Main {
         System.out.println(parseValueDoubleToString(13.56432d));
         System.out.println(parseValueStringToInt("756"));
         System.out.println(parseValueStringToDouble("0.456222554"));
-//        System.out.println(smallWord("Кастинг, горим и гаснем!"));
+        System.out.println(smallWord("Жизнь - Кастинг,горим и гаснем! \n У лукоморья - дуб зеленый, \n Златая цепь на дубе том."));
+
+        Scanner words = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        wordsLine = words.nextLine();
+        System.out.println("Кол-во слов в введённой строке = " + countWords(wordsLine));
+        System.out.println("Новая строка: \n" + deleteOfLastWord(wordsLine));
     }
 
     public int valueSumm(int a, int b) {
@@ -48,6 +53,9 @@ public class Main {
         return a % b;
     }
 
+    ///////////////////////////////////////////////////////
+
+    //чётное число
     public static int valueEvent(int a, int b, int c) {
         int result = 0;
 
@@ -62,6 +70,7 @@ public class Main {
         return result;
     }
 
+    //определение координат
     public static int valueCoordinates(int x, int y){
         int result = 0;
 
@@ -80,10 +89,12 @@ public class Main {
         return result;
     }
 
+    //нахождение факториала
     public static int valueFact(int n){
         return n <= 1 ? 1: n * valueFact(n - 1);
     }
 
+    //зеркальное отображение числа
     public static int valueMirror(int n){
         int tmp = 0;
 
@@ -94,6 +105,7 @@ public class Main {
         return tmp;
     }
 
+    // реверс массива
     public static void reversArray(int[] array){
 
         int border = (array.length + 1)/ 2;
@@ -109,6 +121,7 @@ public class Main {
         }
     }
 
+    // сортировка пузырьком
     public static void bubleSort(int[] array){
         int tmp = 0;
 
@@ -123,6 +136,7 @@ public class Main {
         }
     }
 
+    // парсим целое число в строку
     public static String parseValueIntToString(int a){
         String str = "";
 
@@ -131,6 +145,7 @@ public class Main {
         return str;
     }
 
+    // парсим число с точкой в строку
     public static String parseValueDoubleToString(double a){
         String str = "";
 
@@ -139,6 +154,7 @@ public class Main {
         return str;
     }
 
+    // парсим строку в целое число
     public static int parseValueStringToInt(String a){
         int str = 0;
 
@@ -147,6 +163,7 @@ public class Main {
         return str;
     }
 
+    // парсим строку в число с точкой
     public static double parseValueStringToDouble(String a){
         double str = 0.0d;
 
@@ -155,12 +172,44 @@ public class Main {
         return str;
     }
 
-//    public static String smallWord(String a){
-//        String[] minWord = a.split("[\\p{Punct}\\s]+");
-//        Set set = new TreeSet<String>(/*(o1, o2) -> o1.length() - o2.length()*/);
-//        set.addAll(Arrays.asList(minWord));
-//
-//        String shortest = (String) set.toArray()[0];
-//        return shortest;
-//    }
+    // нахождение минимального слова в строке
+    public static String smallWord(String a){
+        String[] minWord = a.trim().split("[\\s\\p{Punct}]");
+
+        int minIndex = 0;
+
+        for(int i = 0; i < minWord.length; i++){
+            if(minWord[i].length() < minWord[minIndex].length()){
+                if((minWord[i].length() != 0) && (minWord[i].length() != 1)) {
+                    minIndex = i;
+                }
+            }
+        }
+
+        return minWord[minIndex];
+
+    }
+
+    // считаем кол-во слов во введенной строке
+    public static int countWords(String a){
+        int count = 0;
+
+        String[] word = a.trim().split("[\\s\\p{Punct}]");
+
+        for(int i = 0; i < word.length; i++){
+            if((word[i].length() != 0) && (word[i].length() != 1)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // удаляем последнее слово в строке, работает если написали строку правильно)))
+    public static String deleteOfLastWord(String a){
+
+       String word = a.trim().substring(0, a.lastIndexOf(" "));
+
+        return word;
+    }
 }
